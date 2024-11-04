f16865c91d9d33470034646670a0c004b7410976
Roshan Kunmar <anitamishraasansol@gmail.com>
2024-11-04 18:56:31 +0530

    project files

---
+++ src/Components/Profile.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import UserContext from '../Context/UserContext'
+function Profile() {
+    const { user } = React.useContext(UserContext)
+  if(!user) return <div>Please Login</div>
+
+  return <div>Welcome {user.username}</div>
+}
+
+export default Profile
